20dc4c17686a46a744f31fc61f9505fa5019b39b
leomenrex <36630407+leomenrex@users.noreply.github.com>
2022-10-22 14:37:10 +0800

    增加选项

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-# UC文件缓冲路径 例如D:/CloudMusic/Cache/
-UC_PATH = './cache/'
+# 文件路径录入示范： 'D:/CloudMusic/Cache/' 或者 'D:\\CloudMusic\\Cache\\' 
+# 如果要录入windows目录的反斜杠注意转义
+
+UC_PATH = 'D:/CloudMusic/Cache/'
+
 # 生成MP3文件路径
-MP3_PATH = './mp3/'
+MP3_PATH = './result/'
+
+# 每次请求API的文件个数
+STEP_LENGTH = 20
+
+# 请求API时最长重试时间，单位秒
+AWAIT_TIME = 43
